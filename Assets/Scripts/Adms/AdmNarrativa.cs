22e4672e461357b13c34fe04c1449887dab78065
Alexandre Machado <amch.smd@gmail.com>
2019-05-20 03:37:25 -0300

    WIP: Lógica dos diálogos

--- Assets/Scripts/Adms/AdmNarrativa.cs
+++ Assets/Scripts/Adms/AdmNarrativa.cs
@@ -5,50 +5,96 @@ using UnityEngine;
 public class AdmNarrativa : MonoBehaviour
 {
 
-    public GameObject papelMoacir;
-    public GameObject papelOponente;
 
-    public string personagemAtivo = "";
 
-    ArrayList falasMoacir = new ArrayList();
-    ArrayList falasBoto = new ArrayList();
-    ArrayList falasIara = new ArrayList();
+    private Queue<string> frasesMoacir;
+    private Queue<string> frasesBoto;
 
-    int indexMoacir = 0, indexBoto = 0, indexIara = 0;
 
-    void preencherMoacir(){
-        falasMoacir.Add("moacir1");
-        falasMoacir.Add("moacir2");
-        falasMoacir.Add("moacir3");
-        falasMoacir.Add("moacir4");
+    void Start(){
+        frasesMoacir = new Queue<string>();
+        frasesBoto = new Queue<string>();
+
     }
 
-    void preencherBoto(){
-        falasBoto.Add("boto1");
-        falasBoto.Add("boto2");
-        falasBoto.Add("boto3");
-        falasBoto.Add("boto4");
-    }
 
-    public void ProximoDialogo(){
-        switch(personagemAtivo){
-            case "Moacir":
-                indexMoacir += 1;
-                papelMoacir.SetActive(true);
-                papelOponente.SetActive(false);
-                break;
 
-            case "Boto":
-                indexBoto += 1;
-                papelOponente.SetActive(true);
-                papelMoacir.SetActive(false);
-                break;
 
-            case "Iara":
-                indexIara += 1;
-                 papelOponente.SetActive(true);
-                papelMoacir.SetActive(false);
-                break;
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public GameObject papelMoacir;
+    // public GameObject papelOponente;
+
+    // public string personagemAtivo = "";
+
+    // ArrayList falasMoacir = new ArrayList();
+    // ArrayList falasBoto = new ArrayList();
+    // ArrayList falasIara = new ArrayList();
+
+    // int indexMoacir = 0, indexBoto = 0, indexIara = 0;
+
+    // void Start(){
+    //     preencherBoto();
+    //     preencherMoacir();
+    //     papelMoacir.SetActive(false);
+    //     papelOponente.SetActive(false);
+
+    //     personagemAtivo = "Moacir";
+
+    // }
+
+    // void preencherMoacir(){
+    //     falasMoacir.Add("moacir1");
+    //     falasMoacir.Add("moacir2");
+    //     falasMoacir.Add("moacir3");
+    //     falasMoacir.Add("moacir4");
+    // }
+
+    // void preencherBoto(){
+    //     falasBoto.Add("boto1");
+    //     falasBoto.Add("boto2");
+    //     falasBoto.Add("boto3");
+    //     falasBoto.Add("boto4");
+    // }
+
+    // public void ProximoDialogo(){
+    //     switch(personagemAtivo){
+    //         case "Moacir":
+    //             indexMoacir += 1;
+    //             if(indexMoacir > falasMoacir.Size()){
+    //                 indexMoacir = falasMoacir.Size();
+    //             }
+    //             papelMoacir.SetActive(true);
+    //             papelOponente.SetActive(false);
+    //             break;
+
+    //         case "Boto":
+    //             indexBoto += 1;
+    //             papelOponente.SetActive(true);
+    //             papelMoacir.SetActive(false);
+    //             break;
+
+    //         case "Iara":
+    //             indexIara += 1;
+    //             papelOponente.SetActive(true);
+    //             papelMoacir.SetActive(false);
+    //             break;
+    //     }
+    // }
+} 
