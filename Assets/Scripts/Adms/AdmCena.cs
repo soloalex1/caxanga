60b8d748e3988c74b5cd54bc9120930ad0e033d8
Alexandre Machado <amch.smd@gmail.com>
2019-05-25 10:18:11 -0300

    LFS: Imagens e animações de tutorial

--- Assets/Scripts/Adms/AdmCena.cs
+++ Assets/Scripts/Adms/AdmCena.cs
@@ -8,6 +8,12 @@ public class AdmCena : MonoBehaviour
     public GameObject telaConfiguracoes;
     public Sprite cursorIdle;
     // gerenciamento de cenas
+
+    private void Start()
+    {
+        Configuracoes.admCena = this;
+    }
+
     public void CarregarCena(string proximaCena)
     {
         // loadSceneAsync tá demorando mais
