8be7e6be8e8127f60b5be392dca75d5dbcc549ee
Alexandre Machado <amch.smd@gmail.com>
2019-06-02 12:13:32 -0300

    WIP: Layout e lógica da tela de coleção
    Iniciada a implementação da tela de coleção. Estou trabalhando no script que vai colocar as cartas diretamente nas páginas.

---
+++ Assets/Scripts/Adms/AdmColecao.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdmColecao : MonoBehaviour
+{
+    public Baralho baralho = new Baralho();
+    public GameObject prefabCarta, paginaEsquerda, paginaDireita;
+
+    GameObject carta;
+    ExibirInfoCarta infoCarta;
+
+    InstanciaCarta instCarta;
+
+    AdmRecursos ar = Configuracoes.GetAdmRecursos();
+
+    void Start()
+    {
+        for(int i = 0; i < baralho.cartasBaralho.Count; i++ )
+        {
+            carta = Instantiate(prefabCarta) as GameObject;
+            infoCarta = carta.GetComponent<ExibirInfoCarta>();
+            instCarta = carta.GetComponent<InstanciaCarta>();
+            infoCarta.CarregarCarta(ar.obterInstanciaCarta(baralho.cartasBaralho[i]));
+
+            instCarta.carta = infoCarta.carta;
+            instCarta.SetPoderECusto();
+            infoCarta.CarregarCarta(instCarta.carta); 
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void AoClicarBotao(){
+
+    }
+
+}
