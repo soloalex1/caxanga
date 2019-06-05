d7e8a62c5206495ea28586a92e69e25e5588d489
Alexandre Machado <amch.smd@gmail.com>
2019-06-05 20:47:19 -0300

    WIP: Lógica de preenchimento das páginas
    Agora as cartas são colocadas nas páginas de acordo com o grid, com a escala correta e na ordem alfabética. Trabalhando no método de transição de páginas.

--- Assets/Scripts/Adms/AdmColecao.cs
+++ Assets/Scripts/Adms/AdmColecao.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class AdmColecao : MonoBehaviour
 {
-    public Baralho baralho = new Baralho();
+    public Baralho baralho;
     public GameObject prefabCarta, paginaEsquerda, paginaDireita;
 
+    int numPagina = 0;
     GameObject carta;
     ExibirInfoCarta infoCarta;
 
     InstanciaCarta instCarta;
 
-    AdmRecursos ar = Configuracoes.GetAdmRecursos();
+    AdmRecursos ar;
 
     void Start()
     {
-        for(int i = 0; i < baralho.cartasBaralho.Count; i++ )
+        ar = Configuracoes.GetAdmRecursos();
+        InstanciarColecao(0);
+    }
+
+    void InstanciarColecao(int index){
+
+        for(int i = index; i < (index + 8); i++ )
         {
             carta = Instantiate(prefabCarta) as GameObject;
             infoCarta = carta.GetComponent<ExibirInfoCarta>();
             instCarta = carta.GetComponent<InstanciaCarta>();
             infoCarta.CarregarCarta(ar.obterInstanciaCarta(baralho.cartasBaralho[i]));
+            
+            Debug.Log("carreguei a carta " + baralho.cartasBaralho[i].ToString()); 
 
             instCarta.carta = infoCarta.carta;
             instCarta.SetPoderECusto();
-            infoCarta.CarregarCarta(instCarta.carta); 
+            infoCarta.CarregarCarta(instCarta.carta);
+
+            // joga as cartas fisicamente na mão do jogador
+            if((i - index) < 4){
+                Configuracoes.DefinirPaiCarta(carta.transform, paginaEsquerda.transform);
+            } else {
+                numPagina++;
+                Configuracoes.DefinirPaiCarta(carta.transform, paginaDireita.transform);
+            }
+            carta.gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 1);
         }
     }
-
     void Update()
     {
         
